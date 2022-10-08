191434edb192e458c0ccf44cd58f3333e01f9614
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-10-08 21:18:17 +0300

    add 5 steps (second part HW2)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,50 +9,48 @@ export class App extends Component {
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-  filter: '',
-  name: '',
-  number: ''
-  };
+    filter: '',
+    };
 
   addContact = newContact => {
     const newContactEntity = {
       id: nanoid(),
       ...newContact,
     };
-
-    this.setState(state => ({
-           contacts: [...state.contacts, newContactEntity],
-    }))
-    
-    // this.checkNewContactPresence(newContactEntity.name)
-    //   ? alert(`${newContactEntity.name} is already in contacts!`)
-    //   : this.setState(state => ({
-    //       contacts: [...state.contacts, newContactEntity],
-    //     }));
+    this.state.contacts.find(
+      contact =>
+        contact.name.toLowerCase() === newContactEntity.name.toLowerCase()
+    )
+      ? alert(`${newContactEntity.name} is already in contacts!`)
+      : this.setState(state => ({
+          contacts: [...state.contacts, newContactEntity],
+        }));
   };
 
-  handleFilterContactsByName = (e) => {
-    console.dir(e)
-    const { value } = e.target
+  handleFilterContactsByName = e => {
+    const { value } = e.target;
     this.setState(() => ({ filter: value }));
   };
 
   render() {
-
     const contactsByName = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-    
-      return (
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+
+    return (
       <>
         <Section title="Phonebook">
           <Form addContact={this.addContact} />
         </Section>
-          <Section title="Contacts">
-            <Filter filter={this.state.filter} onChange={this.handleFilterContactsByName} />
+        <Section title="Contacts">
+          <Filter
+            filter={this.state.filter}
+            onChange={this.handleFilterContactsByName}
+          />
           <PhoneList contacts={contactsByName} />
         </Section>
       </>
